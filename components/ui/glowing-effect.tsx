e8b513073f4eff31bac0f1aff9ad0581cd1cb812
bowtiedswan <bowtiedswan@gmail.com>
2025-03-25 15:34:31 +0200

    style: update border colors

--- components/ui/glowing-effect.tsx
+++ components/ui/glowing-effect.tsx
@@ -151,9 +151,9 @@ const GlowingEffect = memo(
                   var(--black) calc(25% / var(--repeating-conic-gradient-times))
                 )`
                   : `radial-gradient(circle, #dd7bbb 10%, #dd7bbb00 20%),
-                radial-gradient(circle at 40% 40%, #d79f1e 5%, #d79f1e00 15%),
+                radial-gradient(circle at 40% 40%,rgb(30, 215, 150) 5%, #d79f1e00 15%),
                 radial-gradient(circle at 60% 60%, #5a922c 10%, #5a922c00 20%), 
-                radial-gradient(circle at 40% 60%, #4c7894 10%, #4c789400 20%),
+                radial-gradient(circle at 40% 60%,rgb(173, 67, 189) 10%, #4c789400 20%),
                 repeating-conic-gradient(
                   from 236.84deg at 50% 50%,
                   #dd7bbb 0%,
