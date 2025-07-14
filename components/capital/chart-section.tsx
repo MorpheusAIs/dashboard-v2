162633756d6b6368c5cbcb6cf0eaeb7aa6eb979a
bowtiedswan <bowtiedswan@gmail.com>
2025-07-14 18:45:49 +0200

    refactor: remove unused totalDepositsMOR variable from ChartSection for cleaner code

--- components/capital/chart-section.tsx
+++ components/capital/chart-section.tsx
@@ -12,7 +12,6 @@ export function ChartSection() {
     chartLoading,
     chartError,
     isLoadingHistorical,
-    totalDepositsMOR,
     totalValueLockedUSD,
     currentDailyRewardMOR,
     avgApyRate,
